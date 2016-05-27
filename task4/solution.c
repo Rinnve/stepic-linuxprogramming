5c4e9f273c6c93f869db7810f2a16608bae8f30e
Rinnve <rinnve@yandex.ru>
2016-05-27 13:47:36 +0300

    Task 4 attempt 1

--- task4/solution.c
+++ task4/solution.c
@@ -6,8 +6,23 @@
 ***/
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char** argv) 
 {
-    
+    char buf[255];
+    FILE *f = fopen("/proc/self/status", "r");    
+    while (fgets(buf, sizeof(buf), f))
+    {
+        char tag[255];
+        int ppid = -1;
+        sscanf(buf, "%s\t%i", tag, &ppid);
+        if (strcmp(tag, "PPid:") == 0)
+        {
+            printf("%i\n", ppid);
+            break;
+        }
+    }
+    fclose(f);
+    return 0;
 }
