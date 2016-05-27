147915bb29b23cc0e4b3ec80ed92a9126e7e6f3a
Rinnve <rinnve@yandex.ru>
2016-05-27 14:36:24 +0300

    Task 6 partly done

--- task6/solution.c
+++ task6/solution.c
@@ -7,8 +7,53 @@
 #include <stdio.h>
 #include <string.h>
 
+int getpid()
+{
+    char buf[255];
+    FILE *f = fopen("/proc/self/status", "r");    
+    int pid;
+    while (fgets(buf, sizeof(buf), f))
+    {
+        char tag[255];
+        pid = -1;
+        sscanf(buf, "%s\t%i", tag, &pid);
+        if (strcmp(tag, "Pid:") == 0)        
+            break;
+    }
+    fclose(f);
+    return pid;
+}
+
+int getppid(int pid)
+{
+    char fname[255];
+    sprintf(fname, "/proc/%i/status", pid);
+    char buf[255];
+    FILE *f = fopen(fname, "r");    
+    int ppid;
+    while (fgets(buf, sizeof(buf), f))
+    {
+        char tag[255];
+        ppid = -1;
+        sscanf(buf, "%s\t%i", tag, &ppid);
+        if (strcmp(tag, "PPid:") == 0)        
+            break;
+    }
+    fclose(f);
+    return ppid; 
+}
+
 int main()
 {
-    
+    int pid, ppid;
+    pid = getpid();
+    printf("%i\n", pid);
+    ppid = getppid(pid);
+    while (ppid != 1)
+    {
+        printf("%i\n", ppid);
+        ppid = getppid(ppid);
+    }
+    printf("1\n");
     return 0;
 }
