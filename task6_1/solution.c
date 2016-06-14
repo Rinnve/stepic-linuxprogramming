ac988e7106ed6ce70e2195bc22edac2b321ef9ca
Rinnve <rinnve@yandex.ru>
2016-06-14 18:12:02 +0300

    Task 6.1 seems to be working

--- task6_1/solution.c
+++ task6_1/solution.c
@@ -5,6 +5,11 @@
 ***/
 
 #include <stdio.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 
 int main(int argc, char **argv)
 {
@@ -15,5 +20,13 @@ int main(int argc, char **argv)
     }
     struct hostent *h;
     h = gethostbyname(argv[1]);
+    
+    int i = 0;
+    while (h->h_addr_list[i] != NULL)
+    {   struct in_addr *addr = (struct in_addr *) h->h_addr_list[i];
+        printf("%s\n", inet_ntoa(*addr));
+        i++;
+    }
+    
     return 0;
 }
