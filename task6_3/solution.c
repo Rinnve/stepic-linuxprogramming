b107813bd3030694af18fc26f38c105414cad500
Rinnve <rinnve@yandex.ru>
2016-06-15 15:28:08 +0300

    Task 6.3 seems to be working

--- task6_3/solution.c
+++ task6_3/solution.c
@@ -17,6 +17,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int  chrcomp(const void *p1, const void *p2)
+{
+    return *(char *) p2 - *(char *) p1; 
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 2)
@@ -24,27 +29,28 @@ int main(int argc, char **argv)
         printf("Wrong argument count!\n");
         return 1;
     }
-    /* FROM TASK 6.2
-    int s = socket(AF_INET, SOCK_DGRAM, 0);
+    
     struct sockaddr_in local;
+    int servsock = socket(AF_INET, SOCK_STREAM, 0);    
     inet_aton("127.0.0.1", &local.sin_addr);
     local.sin_port = htons(atoi(argv[1]));
     local.sin_family = AF_INET;
-    int result = bind(s, (struct sockaddr *) &local, sizeof(local));
-    //printf("%i\n", result);
+    bind(servsock, (struct sockaddr *) &local, sizeof(local));    
+    listen(servsock, 5);    
+    int clientsock = accept(servsock, NULL, NULL);
     
     const int BUF_SIZE = 5001;
     char buf[BUF_SIZE];
     while (1)
     {
-        read(s, buf, BUF_SIZE);
-        char *newline_pos = strchr(buf, '\n');
-        if (newline_pos != NULL)
-            *newline_pos = 0;
+        // We should only get zero-terminated string
+        int string_len = read(clientsock, buf, BUF_SIZE) - 1;
+        buf[string_len+1] = 0;     
         if (strcmp(buf, "OFF") == 0) 
             break;
-        printf("%s\n\n", buf);
+        qsort(buf, string_len, sizeof(char), chrcomp);
+        write(clientsock, buf, string_len + 1);                
     }
-    */
+    
     return 0;
 }
